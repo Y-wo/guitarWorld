ae20d7c64dbbc1f4493d1abdbf0636eb8ff8c23c
Y-wo <y-wo@gmx.de>
2024-02-21 08:29:01 +0100

    adjuested sidebar-handling

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,11 +1,34 @@
 document.addEventListener("DOMContentLoaded", async function(){
     let profileButton = document.querySelector('.js-profile');
+    let shoppingCartButton = document.querySelector('.js-shopping-cart');
     let sidebar = document.querySelector('.js-sidebar');
     let isSidebarOpen = false;
     let cross = document.querySelector('.js-sidebar-cross');
+    let loginForm = document.querySelector('.js-login-form');
+    let shoppingForm = document.querySelector('.js-sidebar-shopping-form');
 
-    profileButton.addEventListener('click', function(event){
+
+
+    // Handles Sidebar behavior
+    function handleSidebarByClick(event){
         event.stopPropagation(); 
+        console.log(event.target)
+
+        // Checks, which part of the sidebar has to be displayed
+        if (event.target.classList.contains('js-profile')) {
+            shoppingForm.classList.remove('sidebar__shopping-form--active');
+            loginForm.classList.add('login-form__container--active');
+            profileButton.classList.add('navbar__icon--active');
+            shoppingCartButton.classList.remove('navbar__icon--active');
+            
+        } else {
+            shoppingForm.classList.add('sidebar__shopping-form--active');
+            loginForm.classList.remove('login-form__container--active');
+            shoppingCartButton.classList.add('navbar__icon--active');
+            profileButton.classList.remove('navbar__icon--active');
+        }
+
+        // Opens / Closes sidebar
         if(!isSidebarOpen){
             sidebar.classList.add("active");
             isSidebarOpen = true;
@@ -13,7 +36,17 @@ document.addEventListener("DOMContentLoaded", async function(){
         else {
             sidebar.classList.remove("active");
             isSidebarOpen = false;
+            profileButton.classList.remove('navbar__icon--active');
+            shoppingCartButton.classList.remove('navbar__icon--active');
         }
+    }
+
+    profileButton.addEventListener('click', function(event){
+        handleSidebarByClick(event);
+    });
+
+    shoppingCartButton.addEventListener('click', function(event){
+        handleSidebarByClick(event);
     });
 
     document.addEventListener('click', function(event){
